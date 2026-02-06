84a08e83114903fe12532f1b906eafafecf84042
Philip Salvaggio <salvaggio.philip@gmail.com>
2026-02-06 07:25:02 -0500

    model_rebuild calls after discrimator injections now ignore PydanticUndefinedAnnotation exceptions, allowing for recursive models where the user calls model_rebuild

---
+++ tests/test_recursive_models.py
@@ -0,0 +1,64 @@
+"""A test to cover recursive models with dynapydantic"""
+
+import typing as ty
+
+import dynapydantic
+
+
+def test_recursive_models_without_discriminator_value_generator() -> None:
+    """Tests recursive models without a discriminator value generator"""
+
+    class A(
+        dynapydantic.SubclassTrackingModel,
+        discriminator_field="name",
+    ):
+        """Base class, has discriminator value generator"""
+
+    class B(A):
+        """A concrete non-recursive subclass"""
+
+        name: ty.Literal["B"] = "B"
+        b: int
+
+    class C(A):
+        """Recursive subclass"""
+
+        name: ty.Literal["C"] = "C"
+        c: "AUnion"
+
+    AUnion = A.union()  # noqa: N806
+
+    for cls in A.registered_subclasses().values():
+        cls.model_rebuild()
+
+    model = C(c=C(c=B(b=1)))
+    assert model.c.c.b == 1
+
+
+def test_recursive_models_with_model_value_generator() -> None:
+    """Tests recursive models with a discriminator value generator"""
+
+    class A(
+        dynapydantic.SubclassTrackingModel,
+        discriminator_field="name",
+        discriminator_value_generator=lambda cls: cls.__name__,
+    ):
+        """Base class, has discriminator value generator"""
+
+    class B(A):
+        """A concrete non-recursive subclass"""
+
+        b: int
+
+    class C(A):
+        """Recursive subclass"""
+
+        c: "AUnion"
+
+    AUnion = A.union()  # noqa: N806
+
+    for cls in A.registered_subclasses().values():
+        cls.model_rebuild()
+
+    model = C(c=C(c=B(b=1)))
+    assert model.c.c.b == 1
