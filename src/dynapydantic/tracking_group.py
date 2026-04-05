40fd22f2d9f2474efc80c2bbfda36599a5d99129
Philip Salvaggio <salvaggio.philip@gmail.com>
2026-04-05 12:22:22 -0400

    PR changes

--- src/dynapydantic/tracking_group.py
+++ src/dynapydantic/tracking_group.py
@@ -322,6 +322,16 @@ class TrackingGroup(pydantic.BaseModel):
         NoRegisteredTypesError
             If no types have been registered yet.
         """
+        if annotated is not None:
+            warnings.warn(
+                "The `annotated` parameter is deprecated. Use `plain=True` to "
+                "get a plain union. By default, behavior is governed by "
+                "`union_mode`. Will be removed in a future version.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
+            plain = True if not annotated else plain
+
         n = len(self.models)
         if n == 0:
             msg = (
