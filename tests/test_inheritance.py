555900cc5113e0e73be92003dd4b376e97954d05
Philip Salvaggio <salvaggio.philip@gmail.com>
2026-04-19 09:45:21 -0400

    Swap the test to explicit usage of Polymorphic (where it actually works)

--- tests/test_inheritance.py
+++ tests/test_inheritance.py
@@ -1,5 +1,7 @@
 """Tests for how dynapydantic propagates to subclasses"""
 
+import pydantic
+
 import dynapydantic
 
 
@@ -10,7 +12,6 @@ def test_inheritance_tree_basic() -> None:
         dynapydantic.SubclassTrackingModel,
         discriminator_field="name",
         discriminator_value_generator=lambda cls: cls.__name__,
-        implicit_polymorphic=True,
     ):
         pass
 
@@ -18,12 +19,11 @@ def test_inheritance_tree_basic() -> None:
         dynapydantic.SubclassTrackingModel,
         discriminator_field="name",
         discriminator_value_generator=lambda cls: cls.__name__,
-        implicit_polymorphic=True,
     ):
         pass
 
     class MidA(Base1):
-        a: int
+        pass
 
     class MidB(Base1, exclude_from_union=True):
         pass
@@ -68,3 +68,25 @@ def test_inheritance_tree_basic() -> None:
     assert set(dynapydantic.registered_models(LeafB).values()) == {LeafB}
     assert set(dynapydantic.registered_models(LeafC).values()) == {LeafC}
     assert set(dynapydantic.registered_models(LeafD).values()) == {LeafD}
+
+    for field_t, data, truth in (
+        (Base1, {"name": "MidA"}, MidA()),
+        (Base1, {"name": "LeafA"}, LeafA()),
+        (Base1, {"name": "LeafB"}, LeafB()),
+        (Base1, {"name": "LeafC"}, LeafC()),
+        (MidA, {"name": "MidA"}, MidA()),
+        (MidA, {"name": "LeafA"}, LeafA()),
+        (LeafA, {"name": "LeafA"}, LeafA()),
+        (MidB, {"name": "LeafB"}, LeafB()),
+        (MidB, {"name": "LeafC"}, LeafC()),
+        (LeafB, {"name": "LeafB"}, LeafB()),
+        (LeafC, {"name": "LeafC"}, LeafC()),
+        (Base2, {"name": "LeafC"}, LeafC()),
+        (Base2, {"name": "LeafD"}, LeafD()),
+        (LeafD, {"name": "LeafD"}, LeafD()),
+    ):
+
+        class Model(pydantic.BaseModel):
+            f: dynapydantic.Polymorphic[field_t]
+
+        assert Model(f=data).f == truth
