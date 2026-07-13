98b4c16b15d14f91ea1c3f154f92c9801b228e18
Philip Salvaggio <salvaggio.philip@gmail.com>
2026-07-12 22:35:33 -0400

    Added support for Generic SubclassTrackingModel's

---
+++ tests/test_generics_py312.py
@@ -0,0 +1,34 @@
+"""Unit tests for the generics"""
+
+import sys
+import typing as ty
+
+import pytest
+
+import dynapydantic
+
+T = ty.TypeVar("T")
+
+
+@pytest.mark.skipif(sys.version_info <= (3, 11), reason="Requires Python > 3.12")
+def test_that_generic_subclass_tracking_models_isolate_py312() -> None:
+    """Test that generic SubclassTrackingModels isolate registered models"""
+
+    class Base[T2](
+        dynapydantic.SubclassTrackingModel,
+        discriminator_field="kind",
+        discriminator_value_generator=lambda x: x.__name__,
+    ):
+        """Generic subclass of SubclassTrackingModel"""
+
+        f: T2
+
+    class ConcreteInt(Base[int]):
+        """A concrete subclass of Base[int]"""
+
+    class ConcreteStr(Base[str]):
+        """A concrete subclass of Base[str]"""
+
+    assert dynapydantic.registered_models(Base[int]) == {"ConcreteInt": ConcreteInt}
+    assert dynapydantic.registered_models(Base[str]) == {"ConcreteStr": ConcreteStr}
+    assert dynapydantic.registered_models(Base) == {}
